71923fc7c0f5791fde54e2aef1d83d12c601e512
dariavorom <dariavorom@gmail.com>
2021-01-30 21:04:18 +0300

    feat: emplement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const bracketsArr = bracketsConfig.map(el => el.join(''));
+    for (let i = 0; i < bracketsArr.length; i++) {
+        if (str.includes(bracketsArr[i])) {
+            str = str.replace(bracketsArr[i], '');
+            i = -1;
+        }
+    }
+    return !str;
 }
+
